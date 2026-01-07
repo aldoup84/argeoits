cab0cb73ac6239dfb9a93ebd5f57dcc5d5541645
aldoup84 <aldo.up@culiacan.tecnm.mx>
2026-01-07 16:35:15 -0700

    subiendo proyecto entero
    subiendo el proyecto entero.

---
+++ Assets/Admin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Admin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		string id = PlayerPrefs.GetString("id");
+		string nombre = PlayerPrefs.GetString("nombre");
+		int derechos = PlayerPrefs.GetInt("derechos");
+		// Debug.Log("Id: " + id + "		" + derechos);
+		if (id.Equals("0") && derechos == 0 || nombre.Equals("Admin") && derechos == 0)
+		{
+			GameObject.Find("btnPanelOpciones").SetActive(true);
+			GameObject.Find("btnAcercaDe").SetActive(false);
+		}
+		else {
+			GameObject.Find("btnPanelOpciones").SetActive(false);
+			GameObject.Find("btnAcercaDe").SetActive(true);
+		}
+
+	}
+ 
+}
+
---
+++ Assets/scripts/Focuses.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Focuses : MonoBehaviour
+{
+
+	InputField input1;    //ID
+	InputField AddUser;
+	InputField AddName;
+	InputField addPass;
+	InputField AddEscuela;
+	InputField AddGrupo;
+	InputField AddStatus;
+
+	public void NameFocus() {
+		GameObject.Find("InputNombre").GetComponent<InputField>().ActivateInputField();
+	}
+
+}
